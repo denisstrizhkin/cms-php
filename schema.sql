a576837bdb9c7fd27cc7f134f2b8fac55352c581
Denis Strizhkin <strdenis02@gmail.com>
2024-02-10 02:38:21 +0300

    db schema

---
+++ schema.sql
@@ -0,0 +1,18 @@
+create table users (
+    id       int unsigned auto_increment primary key,
+    username varchar(20)  not null unique,
+    email    varchar(100) not null unique,
+    password varchar(20)  not null,
+    active   bool         not null default true,
+    added    datetime     not null default current_timestamp
+);
+
+create table posts (
+    id        int unsigned auto_increment primary key,
+    title     varchar(50)  not null,
+    content   text         not null,
+    author_id int unsigned not null,
+
+    foreign key (author_id) references users(id) on delete cascade
+);
+
